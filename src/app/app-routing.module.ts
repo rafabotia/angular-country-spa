576453fa71eb449a09bb686ff62a5e52555a72cb
Rafael Botia Ordaz <rafabotiaordaz@gmail.com>
2023-06-05 11:52:34 +0200

    Lazy Load - Carga perezosa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomePageComponent } from './shared/pages/home-page/home-page.component';
 import { AboutPageComponent } from './shared/pages/about-page/about-page.component';
 import { ContactComponent } from './shared/pages/contact-page/contact-page.component';
+import { CountriesRoutingModule } from './countries/countries-routing.module';
 
 const routes: Routes = [
   {
@@ -19,7 +20,7 @@ const routes: Routes = [
   },
   {
     path: 'countries',
-    component: ContactComponent,
+    loadChildren: () => import('./countries/countries.module').then( m => m.CountriesModule )
   },
   {
     path: '**',
