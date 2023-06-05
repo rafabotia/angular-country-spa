d999e1925278d35ed3df2766a3782d885c2a98e4
Rafael Botia Ordaz <rafabotiaordaz@gmail.com>
2023-06-05 11:31:51 +0200

    Countries Routing Module

---
+++ src/app/countries/pages/by-capital-pages/by-capital-pages.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-by-capital-pages',
+  templateUrl: './by-capital-pages.component.html',
+  styles: [
+  ]
+})
+export class ByCapitalPagesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
