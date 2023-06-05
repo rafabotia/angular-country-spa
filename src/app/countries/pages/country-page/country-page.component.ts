e6a3498265ab6b486397eae18097c24f070b579d
Rafael Botia Ordaz <rafabotiaordaz@gmail.com>
2023-06-05 20:25:41 +0200

    Mostrar información del pais

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { CountriesService } from '../../services/countries.service';
 import { switchMap } from 'rxjs';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'app-country-page',
@@ -10,6 +11,8 @@ import { switchMap } from 'rxjs';
   ]
 })
 export class CountryPageComponent implements OnInit {
+
+  public country?: Country;
 
   constructor(
     private activedRoute: ActivatedRoute,
@@ -24,14 +27,8 @@ export class CountryPageComponent implements OnInit {
       switchMap( ({ id }) => this.countriesService.searchCountryByAlphaCode( id ) )
     )
     .subscribe( country => {
-      if ( !country ) {
-        return this.router.navigateByUrl('');
-      }
-
-      console.log('TENEMOS UN PAÍS');
-      console.log({ country });
-      return;
-
+      if ( !country ) return this.router.navigateByUrl('');
+      return this.country = country;
     })
   }
 
