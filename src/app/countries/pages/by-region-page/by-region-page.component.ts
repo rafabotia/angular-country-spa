4d167e08691c8aae7356fc8a5982393970b1b04f
Rafael Botia Ordaz <rafabotiaordaz@gmail.com>
2023-06-07 12:40:10 +0200

    Mantener la data entre pantallas

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Country } from '../../interfaces/country';
 import { CountriesService } from '../../services/countries.service';
 
-type Region = 'Africa'|'Americas'|'Asia'|'Europe'|'Oceania';
+import { Country } from '../../interfaces/country';
+import { Region } from '../../interfaces/region.type';
 
 @Component({
   selector: 'app-by-region-page',
--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -2,11 +2,19 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, catchError, delay, map, of, tap } from 'rxjs';
 import { Country } from '../interfaces/country';
+import { CacheStore } from '../interfaces/cache-store.interface';
 
 @Injectable({providedIn: 'root'})
 export class CountriesService {
 
   private apiUrl: string = 'https://restcountries.com/v3.1'
+
+  public cacheStore: CacheStore = {
+    byCapital:   { term: '', countries: [] },
+    byCountries: { term: '', countries: [] },
+    byRegion:    { region: '' , countries: [] },
+  }
+
 
   constructor(private http: HttpClient) { }
 
