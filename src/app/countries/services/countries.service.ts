8ad67c0321b42a4e9a65551b98fe78f3aa3b8d54
Rafael Botia Ordaz <rafabotiaordaz@gmail.com>
2023-06-07 18:52:01 +0200

    LocalStorage del Store

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -17,7 +17,19 @@ export class CountriesService {
   }
 
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.loadFromLocalStorage();
+  }
+
+  private saveToLocalStorage() {
+    localStorage.setItem( 'cacheStore', JSON.stringify( this.cacheStore ));
+  }
+
+  private loadFromLocalStorage() {
+    if ( !localStorage.getItem('cacheStore') ) return;
+
+    this.cacheStore = JSON.parse( localStorage.getItem('cacheStore')! );
+  }
 
   private getCountriesRequest( url: string): Observable<Country[]> {
     return this.http.get<Country[]>( url )
@@ -46,7 +58,8 @@ export class CountriesService {
     return this.getCountriesRequest(url)
       .pipe(
         // tap( countries => this.cacheStore.byCapital = { term: term, countries: countries } )
-        tap( countries => this.cacheStore.byCapital = { term, countries } )
+        tap( countries => this.cacheStore.byCapital = { term, countries } ),
+        tap( () => this.saveToLocalStorage() ),
       )
   }
 
@@ -56,7 +69,8 @@ export class CountriesService {
 
     return this.getCountriesRequest(url)
       .pipe(
-        tap( countries => this.cacheStore.byCountries = { term, countries } )
+        tap( countries => this.cacheStore.byCountries = { term, countries } ),
+        tap( () => this.saveToLocalStorage() ),
       )
   }
 
@@ -66,7 +80,8 @@ export class CountriesService {
 
     return this.getCountriesRequest(url)
       .pipe(
-        tap( countries => this.cacheStore.byRegion = { region, countries } )
+        tap( countries => this.cacheStore.byRegion = { region, countries } ),
+        tap( () => this.saveToLocalStorage() ),
       )
   }
 }
