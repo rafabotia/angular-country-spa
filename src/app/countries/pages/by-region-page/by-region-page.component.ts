e27e5d49daccb655ca2eb5d5316fa2d6a6749d6d
Rafael Botia Ordaz <rafabotiaordaz@gmail.com>
2023-06-07 12:22:07 +0200

    Buscar por region - Optimización

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Country } from '../../interfaces/country';
 import { CountriesService } from '../../services/countries.service';
 
+type Region = 'Africa'|'Americas'|'Asia'|'Europe'|'Oceania';
+
 @Component({
   selector: 'app-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -11,10 +13,15 @@ import { CountriesService } from '../../services/countries.service';
 export class ByRegionPageComponent {
 
   public countries: Country[] = [];
+  public regions: Region[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+  public selectedRegion?: Region;
 
   constructor( private countriesServices: CountriesService ) {};
 
-  public searchByRegion( region: string ):void {
+  public searchByRegion( region: Region ):void {
+
+    this.selectedRegion = region;
+
     this.countriesServices.searchRegion( region )
       .subscribe( countries => {
         this.countries = countries;
